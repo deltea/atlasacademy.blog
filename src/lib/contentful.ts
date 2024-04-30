cb93e11433c164ed5100ef252f72c03403529839
Leo Feng <thcheetah777@gmail.com>
2024-04-30 09:41:14 -0500

    📦 setup contentful and fetch destinations

---
+++ src/lib/contentful.ts
@@ -0,0 +1,54 @@
+import contentful, { type EntryFieldTypes } from "contentful";
+import type { Continent } from "./utils";
+
+export interface BlogPost {
+  contentTypeId: "blogPost",
+  fields: {
+    title: EntryFieldTypes.Text,
+    slug: EntryFieldTypes.Text,
+    type: EntryFieldTypes.Text,
+    date: EntryFieldTypes.Date,
+    content: EntryFieldTypes.RichText,
+    cover: EntryFieldTypes.AssetLink,
+    destination: EntryFieldTypes.EntryLink<Destination>,
+    podcast: EntryFieldTypes.Text,
+    featured: EntryFieldTypes.Boolean,
+  }
+}
+
+export interface Destination {
+  contentTypeId: "destination",
+  fields: {
+    name: EntryFieldTypes.Text,
+    slug: EntryFieldTypes.Text,
+    short: EntryFieldTypes.Text,
+    cover: EntryFieldTypes.AssetLink,
+    description: EntryFieldTypes.RichText,
+    continent: EntryFieldTypes.Text<Continent>,
+    heading: EntryFieldTypes.Text,
+    code: EntryFieldTypes.Text,
+  }
+}
+
+export interface GalleryPhoto {
+  contentTypeId: "galleryPhoto",
+  fields: {
+    title: EntryFieldTypes.Text,
+    slug: EntryFieldTypes.Text,
+    description: EntryFieldTypes.Text,
+    image: EntryFieldTypes.AssetLink,
+    date: EntryFieldTypes.Date,
+    city: EntryFieldTypes.Text,
+    country: EntryFieldTypes.EntryLink<Destination>
+  }
+}
+
+const contentfulBaseClient = contentful.createClient({
+  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+});
+
+export const contentfulClient = contentfulBaseClient.withoutUnresolvableLinks;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge";
 export function cn(...values: ClassValue[]) {
   return twMerge(clsx(values));
 }
+
+export type Continent = "Asia" | "Americas" | "Africa" | "Europe" | "Middle East";
+export type PostType = "Podcast" | "Reflection";
