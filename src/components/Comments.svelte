a3ac3aebacf7f06c18f83b938f0d42d2e0bf4f50
Leo <thcheetah777@gmail.com>
2024-10-12 18:27:11 -0700

    🔧 fix things

--- src/components/Comments.svelte
+++ src/components/Comments.svelte
@@ -1,13 +1,6 @@
 <script lang="ts">
-  import { Label } from "bits-ui";
   import "iconify-icon";
   import { createEventDispatcher } from "svelte";
-
-  const dispatch = createEventDispatcher<{
-    slug: string;
-    name: string;
-    text: string;
-  }>();
 
   export let comments: {
     id: number;
@@ -32,42 +25,4 @@
       <p class="text-sm text-neutral dark:text-neutral-300">{comment.text}</p>
     </div>
   {/each}
-
-  <form
-    class="w-full space-y-xs"
-    on:submit|preventDefault={(() => dispatch("createComment", { slug, name, text }))}
-  >
-    <div class="space-y-xxs">
-      <Label.Root for="body" class="uppercase tracking-widest font-semibold text-sm font-jost">
-        Add Comment*
-      </Label.Root>
-      <textarea
-        name="Add Comment"
-        class="block w-full outline-none p-2 bg-neutral-100 text-neutral"
-        id="body"
-        rows={5}
-        bind:value={text}
-        required
-      />
-    </div>
-
-    <div class="space-y-xxs">
-      <Label.Root for="author" class="uppercase tracking-widest font-semibold text-sm font-jost">
-        Your Name* 
-      </Label.Root>
-      <input
-        type="text"
-        class="block w-full outline-none p-2 bg-neutral-100 text-neutral"
-        id="author"
-        bind:value={name}
-        required
-      />
-    </div>
-
-    <input
-      type="submit"
-      value="Post Comment"
-      class="uppercase tracking-widest font-jost font-semibold bg-neutral dark:bg-neutral-100 p-xxs px-xs text-sm text-white dark:text-neutral cursor-pointer"
-    />
-  </form>
 </section>
